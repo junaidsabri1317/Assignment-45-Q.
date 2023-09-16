2cc2afe1f559b0ec047518b331918e7f69ba4add
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 20:43:21 +0500

    Assignment 45 Q18

---
+++ q18.js
@@ -0,0 +1,4 @@
+"use strict";
+// Question No.17
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+console.log("I have used 13 of my Friend's Names for these exercises");
---
+++ q18.ts
@@ -0,0 +1,3 @@
+// Question No.17
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+console.log("I have used 13 of my Friend's Names for these exercises")
