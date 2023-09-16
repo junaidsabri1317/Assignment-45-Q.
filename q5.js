8a8db526359e7a1c21c4aadeaf6362ee94a384b6
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 15:55:39 +0500

    Assignment 45 Q4

---
+++ q5.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question No.5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famousPerson = "Sir Dr.Allma Iqbal";
+var quotation = " People who have no hold over their process of thinking ara likely to be ruined by liberty of thought.";
+console.log(famousPerson, "said", quotation);
