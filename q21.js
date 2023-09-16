543df19cbbcb40ca3b70d3b8393da1717dc1cc77
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 20:54:25 +0500

    Assignment 45 Q21

---
+++ q21.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question No.20
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let numbers = [1, 4, 6, 7, 15, 16, 19, 210];
+for (let index = 0; index < numbers.length; index++) {
+    var values = numbers[index];
+    //console.log(numbers[19])// this will  cause and error/undefined value
+    console.log(numbers[index]); // this will print elements of array
+}
