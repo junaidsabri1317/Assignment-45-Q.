1d03ef021b102880ad701cfa60970abd354e1ce8
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 20:48:32 +0500

    Assignment 45 Q19

---
+++ q19.js
@@ -0,0 +1,8 @@
+"use strict";
+// Question No.20
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var cities = ["Faisalabad", "Hasil Pur", "Islamabad", "Karachi", "Bahawal Pur", "Rahim Yar Khan"];
+console.log(cities);
+for (let name of cities) {
+    console.log(name);
+}
