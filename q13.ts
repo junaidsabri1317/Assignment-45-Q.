7115adc82ada36439a11afe13d0bee81f49554ca
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 16:27:23 +0500

    Assignment 45 Q13

---
+++ q13.ts
@@ -0,0 +1,9 @@
+/* Question No.13
+ Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”*/
+
+ var favoriteTransport:string[]=[]
+ favoriteTransport.push("Car","Bike","Scottey","jahaz","Cycle")
+ for (let name of favoriteTransport )
+ {
+     console.log("I love and want to travel on this", name )
+ }
