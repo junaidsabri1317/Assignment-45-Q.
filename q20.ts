b1933439460ff6d95719b2e02f69baece467f4d1
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 20:52:16 +0500

    Assignment 45 Q20

---
+++ q20.ts
@@ -0,0 +1,21 @@
+// Question No.19
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let mydiffPlaces:string[]=["K2","River Indus","Islamabad","Urdu","Pakistan"]
+for (let index = 0; index < mydiffPlaces.length; index++)
+{
+    const element = mydiffPlaces[index];
+    console.log(element)
+}
+
+// *think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+var An_object_laptop = {
+   Data_Darbar : 'lahore',
+   Keyboard:"stylish",
+   Screen:"14 Inch",
+   Storage:"256 GB",
+   RAM:"8 GB",
+   Processor:"Core i5-2 Gen"
+}
+console.log(An_object_laptop)
