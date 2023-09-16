400330a43f826f2f7956ab11eff1373661fa4b07
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 16:11:22 +0500

    Assignment 45 Q8

---
+++ q8.ts
@@ -0,0 +1,4 @@
+//Question No.8
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favrtnumber:number=4
+console.log("My Favourite Number and lucky Number is =", favrtnumber)
