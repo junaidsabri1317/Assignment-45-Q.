7c352b524dafd1031700366c96b6d79194197b31
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 16:02:38 +0500

    Assignment 45 Q7

---
+++ q7.js
@@ -0,0 +1,9 @@
+"use strict";
+//Question No.7
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. You should create four lines that look like this:
+//console.log(8 + 5)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(8 + 5);
+console.log(6 * 4);
+console.log(12 / 4);
+console.log(18 - 6);
