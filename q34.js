5d438df019188cd9e8d311488809d0284906e0f2
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 21:39:30 +0500

    Assignment 45 Q34

---
+++ q34.js
@@ -0,0 +1,6 @@
+"use strict";
+// 34.	T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function..
+function make_shirt(size, txt) {
+    console.log("My Shirt should of size -", size, "- and '", txt, "' should b printed on it");
+}
+make_shirt("Large", "Sports-10");
