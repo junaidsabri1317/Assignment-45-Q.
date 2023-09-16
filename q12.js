0a603bc4dc89f95508655548794d9e2dd1b389a9
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 16:22:57 +0500

    Assignment 45 Q12

---
+++ q12.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question No.12
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var names = ["Adnan Khalil", "Emmanuial James", "Zeeshan ", "Waseem", "Nasir"];
+for (let name of names) {
+    console.log("Hello! Awain  Programers bohat working kerni paray gi atlest 6 hr daily", name);
+}
