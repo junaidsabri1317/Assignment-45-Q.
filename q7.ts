72d6e16bbe7bd4259bee28c62f9be6f17794ac39
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 16:05:23 +0500

    Assignment 45 Q7

--- q7.ts
+++ q7.ts
@@ -2,7 +2,7 @@
 //Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. You should create four lines that look like this:
 //console.log(8 + 5)
 //Your output should simply be four lines with the number 8 appearing once on each line.
-console.log(8+5)
-console.log(6*4)
-console.log(12/4)
-console.log(18-6)
+console.log(7+1)
+console.log(2*4)
+console.log(32/4)
+console.log(14-6)
