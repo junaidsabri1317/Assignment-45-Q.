39bb43a0404d455dcc1ac5379f12f953fec1c96a
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-16 12:32:08 +0500

    Assignment 45 Q4

---
+++ q4.js
@@ -0,0 +1,9 @@
+"use strict";
+// var js:string = "junaid Sabri"
+// console.log("My name is",js);
+//Question No.4
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+let author = "'M.Ali Jinnah'";
+let quote = "'No any power on earth can demolish Pakistan.','Yes Righit 100%.'";
+console.log(author, ", Once Said, ", quote);
